aa1b29edc433674a9abbbed3ce193e09f535d6aa
Atul Chauhan <developer.atulchauhan@gmail.com>
2022-04-20 13:52:15 +0530

    Added Folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TranningComponent } from './tranning/tranning.component';
+import { CurrentTranningComponent } from './tranning/current-tranning/current-tranning.component';
+import { NewTranningComponent } from './tranning/new-tranning/new-tranning.component';
+import { PastTranningComponent } from './tranning/past-tranning/past-tranning.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    TranningComponent,
+    CurrentTranningComponent,
+    NewTranningComponent,
+    PastTranningComponent,
+    WelcomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
